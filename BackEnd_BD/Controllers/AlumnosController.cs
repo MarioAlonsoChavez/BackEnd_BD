f20e5fc0589113e65725e7dda21efc95beb434a0
mario <mario.alonso.chavez.s@gmail.com>
2022-11-05 09:36:04 -0600

    Agregar archivos de proyecto.

---
+++ BackEnd_BD/Controllers/AlumnosController.cs
@@ -0,0 +1,20 @@
+﻿using BackEnd_BD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackEnd_BD.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            using (AlumnoDbContext dbAlumnos =new AlumnoDbContext()) 
+
+            return View(dbAlumnos.Alumnos.ToList());
+        }
+    }
+}
